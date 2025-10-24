03a98b0af30715454124c43c703ce11357fc9238
Astronomax <fgfgfb93@gmail.com>
2025-10-24 17:52:54 +0300

    fixes

--- infrastructure/terraform/secgroup.tf
+++ infrastructure/terraform/secgroup.tf
@@ -32,3 +32,23 @@ resource "vkcs_networking_secgroup_rule" "https_in" {
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = vkcs_networking_secgroup.example.id
 }
+
+resource "vkcs_networking_secgroup_rule" "frontend" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 3000
+  port_range_max    = 3000
+  security_group_id = vkcs_networking_secgroup.example.id
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+resource "vkcs_networking_secgroup_rule" "backend" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 8080
+  port_range_max    = 8080
+  security_group_id = vkcs_networking_secgroup.example.id
+  remote_ip_prefix  = "0.0.0.0/0"
+}
